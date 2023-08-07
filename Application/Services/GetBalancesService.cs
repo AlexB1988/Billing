f76ea04e1e8a8f2dc68cd8496991e123a422ec85
AlexB1988 <bochik_88@mail.ru>
2023-08-08 00:27:09 +0300

    Поправил выборку по периоду - месяц

--- Application/Services/GetBalancesService.cs
+++ Application/Services/GetBalancesService.cs
@@ -20,58 +20,10 @@ namespace Billing.Application.Services
             var balancesTemp = await _balance.GetBalances();
             var payments = await _payment.GetPayments();
 
-            //var result = new List<GetBalancesViewModel>();
-
             var balances = balancesTemp
                 .Where(account => account.AccountId == accountId)
                 .OrderBy(period => period.Period).OrderBy(period => period.Period);
 
-            //var payments = paymentsTemp
-            //    .Where(account => account.AccountId == accountId);
-
-            //decimal paymentPeriodSum=0;
-            //int balancesCounter=0;
-
-
-            //foreach(var balance in balances)
-            //{
-            //    result.Add(new GetBalancesViewModel 
-            //            { 
-            //                Period = DateTime.ParseExact(balance.Period, "yyyyMM", CultureInfo.InvariantCulture),
-            //                AccountId = balance.AccountId,
-            //                InBalance = (balancesCounter !=0) ?(result[balancesCounter - 1].OutBalance):(balance.InBalance),
-            //                Calculate = balance.Calculation
-            //            });
-
-            //    foreach (var payment in payments)
-            //    {
-
-            //        if (result[balancesCounter].Period.Month == payment.Date.Month 
-            //                    && result[balancesCounter].Period.Year == payment.Date.Year)
-            //        {
-            //            paymentPeriodSum += payment.Sum;
-            //        }
-            //    }
-
-            //    result[balancesCounter].Pay = paymentPeriodSum;
-            //    result[balancesCounter].OutBalance = result[balancesCounter].InBalance 
-            //        + result[balancesCounter].Calculate 
-            //        - result[balancesCounter].Pay;
-
-            //    balancesCounter++;
-            //    paymentPeriodSum = 0;
-            //}
-            //var resultYear = result
-            //    .GroupBy(period => period.Period.Year)
-            //    .Select(p =>
-            //        new {
-            //            Period = p.Key,
-            //            p.FirstOrDefault().InBalance,
-            //            Calculate = p.Sum(s => s.Calculate),
-            //            AccountId = p.Min(m => m.AccountId),
-            //            Pay = p.Sum(s => s.Pay),
-            //            p.LastOrDefault().OutBalance
-            //        });
 
             switch (period)
             {
@@ -88,22 +40,18 @@ namespace Billing.Application.Services
                         Calculate = b.Calculation,
                         Pay = p.Where(p => p.Date.Year.ToString() == b.Period.Substring(0, 4)
                                     && p.Date.Month == int.Parse(b.Period.Substring(4)))
-                               .Sum(pay => pay.Sum),
-                        OutBalance = b.InBalance + b.Calculation - p.Where(p => p.Date.Year.ToString() == b.Period.Substring(0, 4)
-                                    && p.Date.Month == int.Parse(b.Period.Substring(4)))
                                .Sum(pay => pay.Sum)
                     }).ToList();
 
-                            int count = 0;
+                    for(int i = 0; i < result.Count(); i++)
+                    {
+                        if (i != 0)
+                        {
+                            result[i].InBalance = result[i - 1].OutBalance;
+                        }
+                        result[i].OutBalance = result[i].InBalance + result[i].Calculate - result[i].Pay;
+                    }
 
-                            foreach (var re in result)
-                            {
-                                if (count != 0)
-                                {
-                                    result[count].InBalance = result[count - 1].OutBalance;
-                                }
-                                count++;
-                            }
                     return result.OrderByDescending(period => period.Period).ToList();
 
                 case Period.Year:
