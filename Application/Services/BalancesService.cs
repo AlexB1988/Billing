300d3e50e86ead3612b97fe0f05a05b3ca4d826a
AlexB1988 <bochik_88@mail.ru>
2023-08-23 22:37:16 +0300

    Изменена реализация switch в методе GetBalances уласса BalaanceService

--- Application/Services/BalancesService.cs
+++ Application/Services/BalancesService.cs
@@ -1,4 +1,5 @@
 ﻿using Billing.Application.Enums;
+using Billing.Application.Exceptions;
 using Billing.Application.Interfaces;
 using Billing.Application.ViewModels;
 
@@ -16,45 +17,42 @@ namespace Billing.Application.Services
         {
             var result =await _balancesPerMonth.GetBalancesPerMonth(accountId);
 
-            switch (period)
+            return period switch
             {
-                case Period.Month:
-                    return result.Select(x => new GetBalancesViewModel
-                        {
-                            AccountId = accountId,
-                            Period = x.Period.ToString("yyyyMM"),
-                            InBalance = x.InBalance,
-                            Calculate = x.Calculate,
-                            Pay = x.Pay,
-                            OutBalance = x.OutBalance
-                        }).OrderByDescending(x => x.Period).ToList();
+                Period.Month => result.Select(x => new GetBalancesViewModel
+                {
+                    AccountId = accountId,
+                    Period = x.Period.ToString("yyyyMM"),
+                    InBalance = x.InBalance,
+                    Calculate = x.Calculate,
+                    Pay = x.Pay,
+                    OutBalance = x.OutBalance
+                }).OrderByDescending(x => x.Period).ToList(),
 
-                case Period.Quater:
-                    return result.GroupBy(x => new { Year = x.Period.Year, Quater = (x.Period.Month - 1) / 3 + 1 })
-                        .Select((x) => new GetBalancesViewModel
-                        {
-                            AccountId = accountId,
-                            Period = x.Key.Year.ToString() + " " + x.Key.Quater.ToString(),
-                            InBalance = x.FirstOrDefault().InBalance,
-                            Calculate = x.Sum(y => y.Calculate),
-                            Pay = x.Sum(y => y.Pay),
-                            OutBalance = x.LastOrDefault().OutBalance
-                        }).OrderByDescending(x => x.Period).ToList();
+                Period.Quater => result.GroupBy(x => new { Year = x.Period.Year, Quater = (x.Period.Month - 1) / 3 + 1 })
+                                    .Select((x) => new GetBalancesViewModel
+                                    {
+                                        AccountId = accountId,
+                                        Period = x.Key.Year.ToString() + " " + x.Key.Quater.ToString(),
+                                        InBalance = x.FirstOrDefault().InBalance,
+                                        Calculate = x.Sum(y => y.Calculate),
+                                        Pay = x.Sum(y => y.Pay),
+                                        OutBalance = x.LastOrDefault().OutBalance
+                                    }).OrderByDescending(x => x.Period).ToList(),
 
-                case Period.Year:
-                    return result.GroupBy(x => new { Year = x.Period.Year })
-                        .Select((x) => new GetBalancesViewModel
-                        {
-                            AccountId = accountId,
-                            Period = x.Key.Year.ToString(),
-                            InBalance = x.FirstOrDefault().InBalance,
-                            Calculate = x.Sum(y => y.Calculate),
-                            Pay = x.Sum(y => y.Pay),
-                            OutBalance = x.LastOrDefault().OutBalance
-                        }).OrderByDescending(x => x.Period).ToList();
-                default:
-                    throw new Exception();
-            }
+                Period.Year => result.GroupBy(x => new { Year = x.Period.Year })
+                                    .Select((x) => new GetBalancesViewModel
+                                    {
+                                        AccountId = accountId,
+                                        Period = x.Key.Year.ToString(),
+                                        InBalance = x.FirstOrDefault().InBalance,
+                                        Calculate = x.Sum(y => y.Calculate),
+                                        Pay = x.Sum(y => y.Pay),
+                                        OutBalance = x.LastOrDefault().OutBalance
+                                    }).OrderByDescending(x => x.Period).ToList(),
+
+                _ => throw new NotFoundException("Некорректно задан период")
+            };
         } 
     }
 }
