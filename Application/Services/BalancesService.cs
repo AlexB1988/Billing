dbd028edd31a58a6cc0294f0923df697275959e8
AlexB1988 <92520323+AlexB1988@users.noreply.github.com>
2023-08-28 14:39:57 +0300

    В методе BalancesService селект вынесен за скобки

--- Application/Services/BalancesService.cs
+++ Application/Services/BalancesService.cs
@@ -1,4 +1,5 @@
-﻿using Billing.Application.Enums;
+﻿using Billing.Application.DTOs;
+using Billing.Application.Enums;
 using Billing.Application.Exceptions;
 using Billing.Application.Interfaces;
 using Billing.Application.ViewModels;
@@ -17,42 +18,32 @@ namespace Billing.Application.Services
         {
             var balance =await _balancesPerMonth.GetBalancesPerMonth(accountId);
 
-            return period switch
+            var resultPerPeriod = period switch
             {
-                Period.Month => balance.Select(x => new BalancesViewModel
-                                    {
-                                        AccountId = accountId,
-                                        Period = x.Period.ToString("yyyyMM"),
-                                        InBalance = x.InBalance,
-                                        Calculate = x.Calculate,
-                                        Pay = x.Pay,
-                                        OutBalance = x.OutBalance
-                                    }).OrderByDescending(x => x.Period).ToList(),
+                Period.Month => balance.GroupBy(x => new { x.Period },
+                                                (x, y) => new { PeriodName = x.Period.ToString("yyyyMM"), Balances = y }),
 
-                Period.Quater => balance.GroupBy(x => new { Year = x.Period.Year, Quater = (x.Period.Month - 1) / 3 + 1 })
-                                    .Select((x) => new BalancesViewModel
-                                    {
-                                        AccountId = accountId,
-                                        Period = x.Key.Year.ToString() + " " + x.Key.Quater.ToString(),
-                                        InBalance = x.FirstOrDefault().InBalance,
-                                        Calculate = x.Sum(y => y.Calculate),
-                                        Pay = x.Sum(y => y.Pay),
-                                        OutBalance = x.LastOrDefault().OutBalance
-                                    }).OrderByDescending(x => x.Period).ToList(),
+                Period.Quater => balance.GroupBy(x => new { Year = x.Period.Year, Quater = (x.Period.Month - 1) / 3 + 1 },
+                                                (x, y) => new { PeriodName =  $"{x.Year} {x.Quater}", Balances = y }),
 
-                Period.Year => balance.GroupBy(x => new { Year = x.Period.Year })
-                                    .Select((x) => new BalancesViewModel
-                                    {
-                                        AccountId = accountId,
-                                        Period = x.Key.Year.ToString(),
-                                        InBalance = x.FirstOrDefault().InBalance,
-                                        Calculate = x.Sum(y => y.Calculate),
-                                        Pay = x.Sum(y => y.Pay),
-                                        OutBalance = x.LastOrDefault().OutBalance
-                                    }).OrderByDescending(x => x.Period).ToList(),
+                Period.Year => balance.GroupBy(x => new { x.Period.Year },
+                                                (x, y) => new {PeriodName = $"{x.Year}", Balances = y}),
 
                 _ => throw new NotFoundException("Некорректно задан период")
             };
+
+
+            var result = resultPerPeriod.Select((x) => new BalancesViewModel
+                                        {
+                                            AccountId = accountId,
+                                            Period = x.PeriodName,
+                                            InBalance = x.Balances.FirstOrDefault().InBalance,
+                                            Calculate = x.Balances.Sum(y => y.Calculate),
+                                            Pay = x.Balances.Sum(y => y.Pay),
+                                            OutBalance = x.Balances.LastOrDefault().OutBalance
+                                        }).OrderByDescending(x => x.Period).ToList();
+
+            return result;
         } 
     }
 }
